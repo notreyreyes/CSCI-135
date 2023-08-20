1ff27d5f0e8876c5c40d9f87a69a3e0d7423d664
Rey Reyes <reyesrey2159@gmail.com>
2023-08-20 13:28:17 -0400

    updated problem 54

--- problem54.cpp
+++ problem54.cpp
@@ -2,6 +2,13 @@
 using namespace std; 
 
 int main(){
-    cout << "Hello world"; 
+    int i, j;
+    for(i = 0; i < 10; i++){
+        for(int j = 0; j < 6; j++){
+            cout << "10"; 
+        }
+    cout << endl; 
+    }
+
 
 }
